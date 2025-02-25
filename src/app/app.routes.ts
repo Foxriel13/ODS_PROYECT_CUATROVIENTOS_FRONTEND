feee5b5d819fb87c86660ec2da88b2a4a2e3e68c
Foxriel13 <luismiguelbravo13@gmail.com>
2025-02-25 09:13:28 +0100

    Crear Modificar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';  // Asegúrate de que estos componentes existen
 import { IniciativasComponent } from './iniciativas/iniciativas.component';
+import { ModificarComponent } from './modificar/modificar.component';
 
 export const routes: Routes = [
     { path: '', component: MenuComponent },
-    { path: 'Iniciativas', component: IniciativasComponent }
+    { path: 'Iniciativas', component: IniciativasComponent },
+    { path: 'Modificar', component: ModificarComponent }
 ];

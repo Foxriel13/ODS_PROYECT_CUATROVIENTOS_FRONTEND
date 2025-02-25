ff2b538074bde04ea999236e7d744f0a14549c2c
Foxriel13 <luismiguelbravo13@gmail.com>
2025-02-25 11:59:50 +0100

    Prueva de remote

--- src/app/modificar/modificar.component.ts
+++ src/app/modificar/modificar.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // ✅ Importar FormsModule
 import { CommonModule } from '@angular/common'; // ✅ Importar CommonModule
+import { CrearIniciativaComponent } from '../crear-iniciativa/crear-iniciativa.component';
+import { ActualizarIniciativaComponent } from '../actualizar-iniciativa/actualizar-iniciativa.component';
+import { EliminarIniciativaComponent } from '../eliminar-iniciativa/eliminar-iniciativa.component';
 
 @Component({
   selector: 'app-modificar',
   standalone: true, // ✅ Indica que es un componente Standalone
-  imports: [CommonModule, FormsModule], // ✅ Agrega FormsModule aquí
+  imports: [CommonModule, FormsModule,CrearIniciativaComponent,ActualizarIniciativaComponent,EliminarIniciativaComponent], // ✅ Agrega FormsModule aquí
   templateUrl: './modificar.component.html',
   styleUrls: ['./modificar.component.scss']
 })
 export class ModificarComponent {
-  iniciativas: string[] = ['']; // Almacena las iniciativas dinámicas
-
-  agregarIniciativa() {
-    this.iniciativas.push(''); // Agrega un nuevo campo de iniciativa vacío
-  }
-
-  guardarODS() {
-    alert("ODS e iniciativas guardadas correctamente.");
-  }
+  opcionSeleccionada: string = 'crear';
 }

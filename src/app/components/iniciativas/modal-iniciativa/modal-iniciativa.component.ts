520b13943d85676366c6a5d375640a5cecd6bffd
karla lasluisa <karla.lasluisa@gmail.com>
2025-02-26 14:24:50 +0100

    diseño modal y coger datos con servicio modal

--- src/app/components/iniciativas/modal-iniciativa/modal-iniciativa.component.ts
+++ src/app/components/iniciativas/modal-iniciativa/modal-iniciativa.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ModalService } from '../../../servicios/modal.service';
 @Component({
   selector: 'app-modal-iniciativa',
   imports: [],
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './modal-iniciativa.component.scss'
 })
 export class ModalIniciativaComponent {
+  titulo: string = "";
+  contratante: string = "";
+  equipoEducativo: string = "";
+  fechaInicio: string = "";
+  fechaFin: string = "";
+  cursos: string[] = [];
+  modulos: string[] = [];
+  metas: string[] = [];
+  producto: string = "";
+  imagenUrl: string = "";
 
+  isOpen = false;
+
+  constructor(private modalService: ModalService) {
+    this.modalService.modalData$.subscribe((data) => {
+      if (data) {
+        // this.titulo = data.titulo;
+        // this.contratante = data.contratante;
+        // this.equipoEducativo = data.equipoEducativo;
+        // this.fechaInicio = data.fechaInicio;
+        // this.fechaFin = data.fechaFin;
+        // this.cursos = data.cursos;
+        // this.modulos = data.modulos;
+        // this.metas = data.metas;
+        // this.producto = data.producto;
+        // this.imagenUrl = data.imagenUrl;
+        this.isOpen = true;
+      } else {
+        this.isOpen = false; // Cuando los datos sean null, cerramos el modal
+      }
+    });
+  }
+
+  close(): void {
+    this.modalService.closeModal();
+  }
 }
---
+++ src/app/servicios/modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Iniciativas } from '../models/iniciativas.model';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ModalService {
+  private modalDataSubject = new BehaviorSubject<{ iniciativa: Iniciativas } | null>(null);
+  modalData$ = this.modalDataSubject.asObservable();
+
+  openModal(iniciativa: Iniciativas): void {
+    this.modalDataSubject.next({ iniciativa });
+  }
+
+  closeModal(): void {
+    this.modalDataSubject.next(null);
+  }
+}

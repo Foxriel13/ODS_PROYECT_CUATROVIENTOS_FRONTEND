65f37b23ec28961cf023f54b519c7eee71998903
AitorLopez057 <alopezbarr1@educacion.navarra.es>
2025-04-10 20:44:10 +0200

    Creado modelo de actividad

---
+++ src/app/models/actividad.model.ts
@@ -0,0 +1,10 @@
+export class Actividad {
+    id: number;
+    nombre: string;
+
+    // Constructor para inicializar la clase
+    constructor(id: number, nombre: string) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
